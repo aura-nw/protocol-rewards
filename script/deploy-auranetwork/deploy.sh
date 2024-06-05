a35b78c4e5cf478c29b753514f1946bbcaf90f3e
HungLV46 <44282860+HungLV46@users.noreply.github.com>
2024-06-05 11:09:56 +0700

    Update deploy.sh

--- script/deploy-auranetwork/deploy.sh
+++ script/deploy-auranetwork/deploy.sh
@@ -1,9 +1,9 @@
 RED_COLOR='\033[0;31m'
 
 echo "Checking env variables..."
-if [ "$RPC_URL" = "" ]; then echo "${RED_COLOR}- Missing rpc RPC_URL env variable"; exit 1; fi
-if [ "$PRIVATE_KEY" = "" ]; then echo "${RED_COLOR}- Missing rpc PRIVATE_KEY env variable"; exit 1; fi
-if [ "$VERIFIER_URL" = "" ]; then echo "${RED_COLOR}- Missing rpc VERIFIER_URL env variable"; exit 1; fi
+if [ "$RPC_URL" = "" ]; then echo "${RED_COLOR}- Missing RPC_URL env variable"; exit 1; fi
+if [ "$PRIVATE_KEY" = "" ]; then echo "${RED_COLOR}- Missing PRIVATE_KEY env variable"; exit 1; fi
+if [ "$VERIFIER_URL" = "" ]; then echo "${RED_COLOR}- Missing VERIFIER_URL env variable"; exit 1; fi
 
 forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY ProtocolRewards \
   --verify --verifier sourcify --verifier-url $VERIFIER_URL
